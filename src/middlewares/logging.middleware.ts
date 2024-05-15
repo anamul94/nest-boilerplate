1c3b99020674f1d47f4735d780fc607f9275171b
Anamul Haque <anamul.ice14@gmail.com>
2024-05-15 11:23:45 +0600

    logging

---
+++ src/middlewares/logging.middleware.ts
@@ -0,0 +1,25 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggingMiddleware implements NestMiddleware {
+  logger = new Logger('Response');
+  constructor() {}
+  use(req: Request, res: Response, next: NextFunction) {
+    const { method, originalUrl: url } = req;
+    const reqTime = new Date().getTime();
+    res.on('finish', () => {
+      const { statusCode } = res;
+      console.log(res);
+      const resTime = new Date().getTime();
+      if (statusCode === 201 || statusCode === 200) {
+        this.logger.log(
+          `${method} ${url} ${statusCode} ${resTime - reqTime}ms`,
+        );
+      } else {
+        this.logger.log(`${method} ${url} ${statusCode}`);
+      }
+    });
+    next();
+  }
+}
