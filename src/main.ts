3a1777b861b4b9741d3b1e7b2d0cd05087f54871
Anamul Haque <anamul.ice14@gmail.com>
2024-05-16 09:45:27 +0600

    OPENTEL

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  require('./config/otel-config');
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
