19e6f1249dc32cdb685225ba6a3e657dd9693c6d
anamul94 <anamul.ice14@gmail.com>
2024-10-28 11:53:41 +0600

    otel config

--- src/config/otel-config.ts
+++ src/config/otel-config.ts
@@ -1,14 +1,18 @@
 /*instrumentation.ts*/
 import { NodeSDK } from '@opentelemetry/sdk-node';
-import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import {
   PeriodicExportingMetricReader,
   ConsoleMetricExporter,
 } from '@opentelemetry/sdk-metrics';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 
 const sdk = new NodeSDK({
-  traceExporter: new ConsoleSpanExporter(),
+  traceExporter: new OTLPTraceExporter({
+    url: 'http://localhost:4318/v1/traces',
+    headers: {},
+    concurrencyLimit: 10, // an optional limit on pending requests
+  }),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new ConsoleMetricExporter(),
   }),
